058770dc3c6f6c567840513462971d0ca6eca211
Sophia <sodoming@ucsd.edu>
2020-01-23 10:45:24 -0800

    m

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -9,10 +9,29 @@ $(document).ready(function() {
  * Function that is called when the document is ready.
  */
 function initializePage() {
-	$("#testjs").click(function(e) {
-		$('.jumbotron h1').text("Javascript is connected");
-	});
+    $('#testjs').click(function(e) {
+            $('.jumbotron h1').text("JavaScript is connected");
+            $("#testjs").text("Please wait...");
+            $(".jumbotron p").toggleClass("active");
+            });
+    $("a.thumbnail").click(projectClick);
+    function projectClick(e) {
+    console.log("Project clicked");
+    // prevent the page from reloading
+     
+    e.preventDefault();
+    // In an event handler, $(this) refers to
+     
+    // the object that triggered the event
+     
 
-	// Add any additional listeners here
-	// example: $("#div-id").click(functionToCall);
+   var containingProject = $(this).closest(".project");
+    var description = $(containingProject).find(".project-description");
+    if (description.length == 0) {
+       $(containingProject).append("<div class='project-description'><p>Description of the project.</p></div>");
+    } else {
+       $(".project-description").hide();
+    }
+    
+}
 }
